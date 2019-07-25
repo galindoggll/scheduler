752cf1f77a5cb16b297be4ff918dbd7e56709f25
galindo.ggll <galindo.ggll@sprobe.com>
2019-07-25 10:45:56 +0800

    Implement CRUD of Company, Client and Staff

--- database/migrations/2019_07_17_032139_create_companies_table.php
+++ database/migrations/2019_07_17_032139_create_companies_table.php
@@ -17,10 +17,11 @@ class CreateCompaniesTable extends Migration
             $table->bigIncrements('id');
             $table->string('name', 255);
             $table->string('contact_no', 15);
-            $table->string('email', 15);
-            $table->string('business_permit', 15)->nullable();
-            $table->string('address', 15);
+            $table->string('email', 50);
+            $table->string('business_permit', 50)->nullable();
+            $table->string('address', 100);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
